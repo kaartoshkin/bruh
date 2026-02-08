a5c5cd524e474bf75e14afb87795c204940b3b07
kaartoshkin <kaartoshkin@yandex.ru>
2026-02-08 23:26:10 +0300

    Refactor run_test.ps1 to simplify allure report generation and serving

--- run_test.ps1
+++ run_test.ps1
@@ -3,9 +3,7 @@ param (
 )
 
 Remove-Item -Recurse -Force reports\allure-results -ErrorAction SilentlyContinue
-Remove-Item -Recurse -Force reports\allure-report -ErrorAction SilentlyContinue
 
 python -m pytest $TestPath --alluredir=reports\allure-results
 
-allure generate reports\allure-results -o reports\allure-report --clean
-allure open reports\allure-report
+allure serve reports\allure-results
