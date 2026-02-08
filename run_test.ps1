55b0975b6a711b60f85e27fd0e35b03947107241
kaartoshkin <kaartoshkin@yandex.ru>
2026-02-08 22:58:55 +0300

    Update run_test.ps1 to generate and open allure report after tests

--- run_test.ps1
+++ run_test.ps1
@@ -3,7 +3,9 @@ param (
 )
 
 Remove-Item -Recurse -Force reports\allure-results -ErrorAction SilentlyContinue
+Remove-Item -Recurse -Force reports\allure-report -ErrorAction SilentlyContinue
 
 python -m pytest $TestPath --alluredir=reports\allure-results
 
-allure serve reports\allure-results
+allure generate reports\allure-results -o reports\allure-report --clean
+allure open reports\allure-report
