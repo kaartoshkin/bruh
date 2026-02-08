3ce3338231cf3ec925fdf7e0d72d574929ab592a
kaartoshkin <kaartoshkin@yandex.ru>
2026-02-08 22:55:55 +0300

    Add cleanup step to remove previous allure results before running tests

--- run_test.ps1
+++ run_test.ps1
@@ -2,6 +2,8 @@ param (
     [string]$TestPath
 )
 
+Remove-Item -Recurse -Force reports\allure-results -ErrorAction SilentlyContinue
+
 python -m pytest $TestPath --alluredir=reports\allure-results
 
 allure serve reports\allure-results
